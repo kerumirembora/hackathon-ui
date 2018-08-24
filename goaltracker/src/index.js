27bd0935ee15b9818ff691d692c85a4e233f46b1
Daniel Paula <daniel.paula@sbanken.no>
2018-08-24 16:16:09 +0200

    import materialize-css in the app

--- goaltracker/src/index.js
+++ goaltracker/src/index.js
@@ -1,3 +1,6 @@
+import './../node_modules/materialize-css/dist/css/materialize.min.css';
+import './../node_modules/materialize-css/dist/js/materialize.min.js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
