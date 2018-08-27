7031fa718926a498a4b22af64992ebc51da5c296
Daniel Paula <daniel.paula@sbanken.no>
2018-08-27 19:24:32 +0200

    mock 3 categories and create the template

--- goaltracker/src/components/GoalsCategories/GoalsCategories.jsx
+++ goaltracker/src/components/GoalsCategories/GoalsCategories.jsx
@@ -2,19 +2,36 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 import MenuWrapper from './../MenuWrapper';
+import './GoalsCategories.scss';
 
 const categories = [
-    { id: 1, name: "Social" },
-    { id: 2, name: "Trip" },
-    { id: 3, name: "Curse Jar" }
+    { id: 1, name: "Social", img: "https://financialtribune.com/sites/default/files/styles/slideshow/public/field/image/shahrivar1/12_Social%20Media%20%281%29.jpg?itok=WU4SVOlO&c=a30dbb2db167c853d8c65a6b85b89f8a" },
+    { id: 2, name: "Trip", img: "https://media.timeout.com/images/101594229/630/472/image.jpg" },
+    { id: 3, name: "Curse Jar", img: "https://therooster.com/sites/default/files/userfiles/images/SwearJargif.gif" }
 ];
+
+const styleCardItem = {
+    // height: "173px",
+    // overflow: "hidden"
+};
+
+const GoalsCategoriesItem = ({ name, id, img }) => (
+    <div className="card" style={styleCardItem}>
+        <div className="card-image">
+          <img src={img} />
+          <span className="card-title">{name}</span>
+        </div>
+    </div>
+);
 
 class GoalsCategoriesComponent extends React.Component {
     render() {
         return (
             <MenuWrapper heading="Goals Categories">
-                <div>
-                    categories!!
+                <div className="categories-wrapper">
+                    {categories.map(cat => 
+                        <GoalsCategoriesItem name={cat.name} id={cat.id} img={cat.img} key={ "categoryElement_" + cat.id } />
+                    )}
                 </div>
             </MenuWrapper>
         );        
@@ -24,7 +41,8 @@ class GoalsCategoriesComponent extends React.Component {
 const mapStateToProps = state => ( {} );
 
 const mapDispatchToProps = dispatch => {
-  return {    
+  return {
+      // add function to go to the category details
   }
 };
 
--- goaltracker/src/components/MenuWrapper/MenuWrapper.jsx
+++ goaltracker/src/components/MenuWrapper/MenuWrapper.jsx
@@ -15,8 +15,8 @@ class MenuWrapper extends React.Component {
         <div>
           <nav style={{ marginBottom: "0px" }}>
             <div className="nav-wrapper">
-              <a href="#" className="brand-logo center">STracker</a>
-              <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+              <a href="/" className="brand-logo center">STracker</a>
+              <a href="/" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
               <ul id="nav-mobile" className="sidenav">
                 <li><a href="sass.html">Menu Item 1</a></li>
                 <li><a href="sass.html">Menu Item 2</a></li>
